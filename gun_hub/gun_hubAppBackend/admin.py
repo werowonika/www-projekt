e9345a652292b497e130be026c037b720df8c60c
Drym <106377232+Olej05@users.noreply.github.com>
2024-02-22 11:30:50 +0100

    pierwszy commit aplikacji

---
+++ gun_hub/gun_hubAppBackend/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib import admin
+
+from .models import Cal, Category, Company, Product
+
+
+class CalAdmin(admin.ModelAdmin):
+    list_display = ["cal"]
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["com"]
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "cal", "com", "category"]
+
+
+# Register your models here.
+admin.site.register(Cal, CalAdmin)
+admin.site.register(Category)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Product, ProductAdmin)
---
+++ gun_hub/gun_hubAppBackend/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.db.models import Model
+
+# Create your models here.
+class Cal(models.Model):
+    cal = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.cal
+
+
+
+
+class Company(models.Model):
+    com = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.com
+
+
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+    def __str__(self):
+        return self.category_name
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    cal = models.ForeignKey(Cal, on_delete = models.CASCADE, null=True)
+    com = models.ForeignKey(Company, on_delete = models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, null=True)
+    year = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name} {self.cal} {self.com} {self.category}'
+
